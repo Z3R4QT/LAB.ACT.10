bae5ff8c083fe0a3d785a91d9d1a1928a33fc679
Z3R4QT <christopher092480@gmail.com>
2025-11-07 18:26:13 +0800

    dpa tapos

--- LAB.ACT.10/Form1.vb
+++ LAB.ACT.10/Form1.vb
@@ -1,3 +1,60 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Linq
 
+Public Class Form1
+    Dim filePath As String = "numbers.txt"
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+
+        Dim num As Integer = CInt(NumericUpDown1.Value)
+
+            Dim writer As StreamWriter = New StreamWriter(filePath, True)
+            writer.WriteLine(num)
+        writer.Close()
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        ListBox1.Items.Clear()
+
+        If File.Exists(filePath) Then
+            Dim reader As StreamReader = New StreamReader(filePath)
+            Dim line As String = reader.ReadLine()
+
+            While line IsNot Nothing
+                ListBox1.Items.Add(line)
+                line = reader.ReadLine()
+            End While
+
+            reader.Close()
+        End If
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        If File.Exists(filePath) Then
+            Dim numbers As New List(Of Integer)
+            Dim reader As StreamReader = New StreamReader(filePath)
+            Dim line As String = reader.ReadLine()
+
+            While line IsNot Nothing
+                numbers.Add(CInt(line))
+                line = reader.ReadLine()
+            End While
+            reader.Close()
+
+            Dim sortedNumbers = From n In numbers
+                                Order By n Ascending
+                                Select n
+
+            ListBox1.Items.Clear()
+            For Each n As Integer In sortedNumbers
+                ListBox1.Items.Add(n)
+            Next
+
+            Dim writer As StreamWriter = New StreamWriter(filePath, False)
+            For Each n As Integer In sortedNumbers
+                writer.WriteLine(n)
+            Next
+            writer.Close()
+        End If
+    End Sub
 End Class
